9737599486d8a9dde59d0e1031dab78738bdc138
ErlingMMM <71938112+ErlingMMM@users.noreply.github.com>
2022-01-28 17:21:56 +0100

    github actions

--- application.jsx
+++ application.jsx
@@ -1,20 +1,34 @@
-import * as React from "react";
+import React, {useState} from "react";
 
 export function ListMovies() {
-    return <div><ul><h1>List Movies</h1></ul></div>;
+    return <div><h1>List movies</h1>
+        <ul>
+            <li>Movie 1</li>
+            <li>Movie 2</li>
+        </ul>
+    </div>;
 }
 
-export function NewMovieForm() {
-    return <form>
+export function NewMovieForm({onAddMovie}) {
+    const [title, setTitle] = useState("");
+    const [year, setYear] = useState("");
+    const [plot, setPlot] = useState("");
+
+    function handleSubmit() {
+        onAddMovie({title, year, plot});
+    }
+
+    return <form onSubmit={handleSubmit}>
         <h1>Add new movie</h1>
         <div>
-            Title: <input/>
+            Title: <input data-testid="title" value={title} onChange={e => setTitle(e.target.value)}/>
         </div>
         <div>
-            Year: <input/>
+            Year: <input data-testid="year" value={year} onChange={e => setYear(e.target.value)}/>
         </div>
         <div>
-            Plot: <textarea></textarea>
+            Plot:
+            <div><textarea data-testid="plot"/></div>
         </div>
     </form>;
 }
